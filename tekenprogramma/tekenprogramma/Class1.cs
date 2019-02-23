1a1a0d08cea6fd3ba5b54a1109d6f23cc106ddd2
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-02-23 14:17:57 +0100

    command
    command

---
+++ tekenprogramma/tekenprogramma/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tekenprogramma
+{
+    public class Shape {
+        public void Create()
+        {
+
+
+        }
+    }
+
+    class CreateRectangle
+    {
+        private Shape rectangle;
+
+        public CreateRectangle(Shape rectangle)
+        {
+            this.rectangle = rectangle;
+        }
+
+        public void execute()
+        {
+            rectangle.Create();
+        }
+    }
+
+    class CreateElipse
+    {
+        private Shape elipse;
+
+        public CreateElipse(Shape elipse)
+        {
+            this.elipse = elipse;
+        }
+
+        public void execute()
+        {
+            elipse.Create();
+        }
+    }
+
+    class CreateShape
+    {
+    }
+
+}
