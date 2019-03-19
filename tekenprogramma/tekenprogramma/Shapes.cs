1a698b59d6aab6683e0f7abe4bf2538f3721e59f
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-03-19 11:31:46 +0100

    basis2
    basis2

---
+++ tekenprogramma/tekenprogramma/Shapes.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tekenprogramma
+{
+    
+    public class Shape {
+        public void Create()
+        {
+
+
+        }
+    }
+    
+    /*
+    public class CreateRectangle
+    {
+        private Shape rectangle;
+        private int leftCoord;
+        private int topCoord;
+        private int rightCoord;
+        private int bottomCoord;
+        //private string actionType;
+        public string actionType { get; set; }
+
+        public CreateRectangle(Shape rectangle)
+        {
+            this.rectangle = rectangle;
+        }
+
+        public void execute()
+        {
+            rectangle.Create();
+        }
+
+        public void OnPointerPressed()
+        {
+            this.actionType = "selected";
+        }
+    }
+    */
+
+    public class CreateElipse
+    {
+        private Shape elipse;
+        private int leftCoord;
+        private int topCoord;
+        private int rightCoord;
+        private int bottomCoord;
+        //private string actionType;
+        public string actionType { get; set; }
+
+        public CreateElipse(Shape elipse)
+        {
+            this.elipse = elipse;
+        }
+
+        public void execute()
+        {
+            elipse.Create();
+        }
+
+        public void OnPointerPressed()
+        {
+            this.actionType = "selected";
+        }
+    }
+
+    class CreateShape
+    {
+    }
+
+}
