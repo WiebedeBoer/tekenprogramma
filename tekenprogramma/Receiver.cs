0237bc6fb351aa8a85ab9ac938f07eacf099322b
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-03-26 14:18:25 +0100

    stap2 v0.1
    stap2 v.01

---
+++ tekenprogramma/Receiver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tekenprogramma
+{
+    public class Receiver
+    {
+        private string action;
+
+        public Receiver()
+        {
+        }
+
+        void Actions(string action)
+        {
+            this.action = action;
+        }
+    }
+
+    public class ConcreteCommand : ICommand
+    {
+        public ConcreteCommand()
+        {
+        }
+
+        void ICommand.Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface ICommand
+    {
+        void Execute();
+    }
+}
---
+++ tekenprogramma/Shapes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tekenprogramma
+{
+
+    public class Shape
+    {
+        public void Create()
+        {
+
+
+        }
+    }
+
+    //public class MouseBinding : System.Windows.Input.InputBinding;
+    public class CreateRectangle
+    {
+        private Shape rectangle;
+        public int leftCoord;
+        public int topCoord;
+        public int rightCoord;
+        public int bottomCoord;
+        //private string actionType;
+        public string actionType { get; set; }
+
+        public CreateRectangle()
+        {
+            actionType = actionType;
+        }
+
+
+        public void execute()
+        {
+            rectangle.Create();
+        }
+
+        public void OnPointerPressed()
+        {
+            this.actionType = "selected";
+        }
+    }
+
+    public class CreateElipse
+    {
+        private Shape elipse;
+        private int leftCoord;
+        private int topCoord;
+        private int rightCoord;
+        private int bottomCoord;
+        //private string actionType;
+        public string actionType { get; set; }
+
+        public CreateElipse(Shape elipse)
+        {
+            this.elipse = elipse;
+        }
+
+        public void execute()
+        {
+            elipse.Create();
+        }
+
+        public void OnPointerPressed()
+        {
+            this.actionType = "selected";
+        }
+    }
+
+    class CreateShape
+    {
+    }
+
+}
